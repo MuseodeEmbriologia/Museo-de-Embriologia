cb6a974e29e0a9aba2036f8b051e100f376e5c1a
MuseodeEmbriologia <museo.de.embriologiaib@gmail.com>
2025-11-23 11:45:46 -0700

    Rotando el modelo

--- Codigos/JavaScriptMuseo.js
+++ Codigos/JavaScriptMuseo.js
@@ -179,7 +179,7 @@ startBtn.addEventListener('click', () => {
     model.setAttribute('gltf-model', 'Modelos y animaciones/Modeloacomodado.glb');
     model.setAttribute('scale', '0.05 0.05 0.05');
     model.setAttribute('position', '0 0 0');
-    model.setAttribute('rotation', '0 0 0');
+    model.setAttribute('rotation', '-90 0 0');
     model.setAttribute('gesture-handler', '');
 
 model.addEventListener('model-loaded', () => {
@@ -221,6 +221,7 @@ exitBtn.addEventListener('click', () => {
         setTimeout(() => { window.location.reload(); }, 300); /*Este script es el que hace que vuelva a la página principal*/
   });
   
+
 
 
 
