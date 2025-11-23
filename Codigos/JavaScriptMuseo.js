5cf829d4333c925c66b1b11ac1f213975e6474e3
MuseodeEmbriologia <museo.de.embriologiaib@gmail.com>
2025-11-23 10:41:46 -0600

    Update JavaScriptMuseo.js

--- Codigos/JavaScriptMuseo.js
+++ Codigos/JavaScriptMuseo.js
@@ -177,7 +177,7 @@ startBtn.addEventListener('click', () => {
     const model = document.createElement('a-entity');
     model.setAttribute('gltf-model', 'Modelos y animaciones/Testiculo21.glb');
     model.setAttribute('scale', '0.1 0.1 0.1');
-    model.setAttribute('position', '0 -0.20 0');
+    model.setAttribute('position', '0 0 0');
     model.setAttribute('rotation', '-90 0 0');
     model.setAttribute('gesture-handler', '');
 
@@ -208,6 +208,7 @@ exitBtn.addEventListener('click', () => {
         setTimeout(() => { window.location.reload(); }, 300); /*Este script es el que hace que vuelva a la página principal*/
   });
   
+
 
 
 
