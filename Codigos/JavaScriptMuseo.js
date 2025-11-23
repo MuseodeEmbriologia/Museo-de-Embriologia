396c666f6b0330a7147695baaa8275e0326b0fe4
MuseodeEmbriologia <museo.de.embriologiaib@gmail.com>
2025-11-23 12:20:36 -0600

    Update JavaScriptMuseo.js

--- Codigos/JavaScriptMuseo.js
+++ Codigos/JavaScriptMuseo.js
@@ -175,12 +175,18 @@ startBtn.addEventListener('click', () => {
     marker.setAttribute('url', 'markers/pattern-marcaCreeper.patt');
 
     const model = document.createElement('a-entity');
-    model.setAttribute('gltf-model', 'Modelos y animaciones/Testiculo27.glb');
+    model.setAttribute('gltf-model', 'Modelos y animaciones/Testiculofinal.glb');
     model.setAttribute('scale', '0.05 0.05 0.05');
     model.setAttribute('position', '0 0 0');
     model.setAttribute('rotation', '-90 0 0');
     model.setAttribute('gesture-handler', '');
-
+    model.addEventListener('model-loaded', (e) => {
+      const object3D = model.getObject3D('mesh');
+      if (object3D) {
+        const box = new THREE.Box3().setFromObject(object3D);
+        const center = box.getCenter(new THREE.Vector3());
+        object3D.position.sub(center);  
+        console.log("Modelo centrado en el marcador");
     marker.appendChild(model);
     scene.appendChild(marker);
 
@@ -208,6 +214,7 @@ exitBtn.addEventListener('click', () => {
         setTimeout(() => { window.location.reload(); }, 300); /*Este script es el que hace que vuelva a la página principal*/
   });
   
+
 
 
 
