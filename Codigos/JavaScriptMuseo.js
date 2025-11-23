1dd2379c54be391afeea7604670bd1c54cfeeae2
MuseodeEmbriologia <museo.de.embriologiaib@gmail.com>
2025-11-22 18:31:01 -0600

    Refactor AR initialization and exit button logic

--- Codigos/JavaScriptMuseo.js
+++ Codigos/JavaScriptMuseo.js
@@ -1,34 +1,43 @@
 const arContainer = document.getElementById('ar-container');
 const startBtn = document.getElementById('start-ar');
 const exitBtn = document.getElementById('exit-ar');
-  // Iniciar AR al presionar el botón del modal
-  startBtn.addEventListener('click', () => {
-    exitBtn.style.display = 'flex';
+
+// Iniciar AR
+startBtn.addEventListener('click', () => {
+
+    arContainer.style.display = 'block';  
+
+    exitBtn.style.display = 'flex';
+
     const scene = document.createElement('a-scene');
     scene.setAttribute('embedded', '');
     scene.setAttribute('vr-mode-ui', 'enabled: false');
     scene.setAttribute('renderer', 'logarithmicDepthBuffer: true');
     scene.setAttribute('arjs', 'sourceType: webcam; debugUIEnabled: false;');
+
     const marker = document.createElement('a-marker');
     marker.setAttribute('type', 'pattern');
     marker.setAttribute('patternUrl', 'markers/pattern-marcaEs.patt');
+
     const model = document.createElement('a-entity');
     model.setAttribute('gltf-model', 'Modelos y animaciones/transparente.glb');
     model.setAttribute('scale', '0.1 0.1 0.1');
+
     marker.appendChild(model);
     scene.appendChild(marker);
+
     const camera = document.createElement('a-entity');
     camera.setAttribute('camera', '');
     scene.appendChild(camera);
+
     arContainer.innerHTML = '';
     arContainer.appendChild(scene);
 });
+
+// Salir
 exitBtn.addEventListener('click', () => {
     arContainer.innerHTML = '';
     arContainer.style.display = 'none';
     exitBtn.style.display = 'none';
-        setTimeout(() => { window.location.reload(); }, 300); /*Este script es el que hace que vuelva a la página principal*/
-  });
-  
-
-
+    setTimeout(() => { window.location.reload(); }, 300);
+});
