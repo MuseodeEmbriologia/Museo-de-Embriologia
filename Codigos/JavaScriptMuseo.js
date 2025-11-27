7487924e7f155cf6763dd5aaa0245007423ec4ed
MuseodeEmbriologia <museo.de.embriologiaib@gmail.com>
2025-11-26 21:24:40 -0600

    Moviendole al yaba

--- Codigos/JavaScriptMuseo.js
+++ Codigos/JavaScriptMuseo.js
@@ -176,7 +176,7 @@ startBtn.addEventListener('click', () => {
 
     const model = document.createElement('a-entity');
     model.setAttribute('gltf-model', 'Modelos y animaciones/Testiculofinal.glb');
-    model.setAttribute('scale', '0.05 0.05 0.05');
+    model.setAttribute('scale', '0.2 0.2 0.2');
     model.setAttribute('rotation', '0 0 0');
     model.setAttribute('position', '0 0 0');
     model.setAttribute('gesture-handler', '');
@@ -210,6 +210,7 @@ exitBtn.addEventListener('click', () => {
         setTimeout(() => { window.location.reload(); }, 300); /*Este script es el que hace que vuelva a la página principal*/
   });
   
+
 
 
 
