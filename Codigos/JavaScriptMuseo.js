adf991b03118ccca3d1dd06a90722ecd919cf55b
MuseodeEmbriologia <museo.de.embriologiaib@gmail.com>
2025-11-27 08:16:22 -0700

    Update JavaScriptMuseo.js

--- Codigos/JavaScriptMuseo.js
+++ Codigos/JavaScriptMuseo.js
@@ -193,7 +193,8 @@ scene.appendChild(marker);
       marker2.setAttribute('url', 'markers/pattern-marcaCreeper.patt');
       
       const model2 = document.createElement('a-entity');
-      model2.setAttribute('gltf-model', 'models/embriologia/ARM3.glb');
+      model2.setAttribute('gltf-model', 'https://github.com/Jesus-2705/prueba2.github.io/raw/refs/heads/main/models/embriologia/ARM3.glb');
+    //https://raw.githubusercontent.com/KelvinRiv/Proyecto/main/RedSeminifera.glb
       model2.setAttribute('scale', '0.1 0.1 0.1');
       model2.setAttribute('rotation', '-90 0 0');
       model2.setAttribute('gesture-handler', '');
@@ -205,7 +206,7 @@ scene.appendChild(marker);
       marker3.setAttribute('url', 'markers/pattern-marcaEs.patt');
       
       const model3 = document.createElement('a-entity');
-      model3.setAttribute('gltf-model', 'models/embriologia/Espermatogenesis5.glb');
+      model3.setAttribute('gltf-model', 'https://github.com/Jesus-2705/prueba2.github.io/raw/refs/heads/main/models/embriologia/Espermatogenesis5.glb');
       model3.setAttribute('scale', '0.1 0.1 0.1');
       model3.setAttribute('rotation', '-90 0 0');
       model3.setAttribute('gesture-handler', '');
@@ -239,6 +240,7 @@ exitBtn.addEventListener('click', () => {
         setTimeout(() => { window.location.reload(); }, 300); /*Este script es el que hace que vuelva a la página principal*/
   });
   
+
 
 
 
